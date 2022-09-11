e8c5a7dc1d7a9c80e5a1ff4083b114b709a8a120
Anderson Carneiro <andersonandradecarneiro@gmail.com>
2022-09-11 12:14:06 -0300

    Heroku new code

--- backend/src/main/java/com/devsuperior/DSmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/DSmeta/services/SmsService.java
@@ -34,7 +34,8 @@ public class SmsService {
 		
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 		
-		String msg = "O Vendedor " + sale.getSeller_name() + " foi destaque em " + date + " com um total de R$ " + String.format("%.2f", sale.getAmount()); 
+		String msg = "O Vendedor " + sale.getSeller_name() + " foi destaque em " + date + 
+				" com um total de R$ " + String.format("%.2f", sale.getAmount()); 
 		
 		Twilio.init(twilioSid, twilioKey);
 
